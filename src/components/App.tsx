ba582ac8583c6bfeeef96270fff314481fa08a3d
artemslt <ppt.artem@gmail.com>
2022-11-20 20:27:06 +0200

    try to fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,8 +10,8 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/winner" element={<Winner />} />
+        <Route path="game" element={<Game />} />
+        <Route path="winner" element={<Winner />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
